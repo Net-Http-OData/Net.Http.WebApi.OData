dc2da7da783eb199382956fc5e1a8f4c2a8fa0fc
Trevor Pilley <trev@trevorpilley.co.uk>
2017-02-20 13:01:00 +0000

    Built Net.Http.WebApi.OData 3.4.1

--- Net.Http.WebApi.OData/Properties/AssemblyInfo.cs
+++ Net.Http.WebApi.OData/Properties/AssemblyInfo.cs
@@ -47,8 +47,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("3.4.0.0")]
 [assembly: AssemblyVersion("3.4.0.0")]
-[assembly: AssemblyFileVersion("3.4.0.0")]
-[assembly: AssemblyInformationalVersion("3.4.0.237")]
+[assembly: AssemblyFileVersion("3.4.1.0")]
+[assembly: AssemblyInformationalVersion("3.4.1.242")]
 [assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7")]
 [assembly: InternalsVisibleTo("Net.Http.WebApi.OData.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010099315584ff23b0d08b9120b4ae0da0c7dd9eb581f7b9f92dba164c86a5825dc53d147adf436c0ec9b2584ffdee592b2bb82367f90942dcc020b2ef21169eee345c623446e41dae7ce58e981c8bbcadb7866724028d8a73b321bd65b0536a23715d1b6221be7ef2f6cc37d9b42c0cd892bda118f59453447a39d33d992a23c5ce")]
