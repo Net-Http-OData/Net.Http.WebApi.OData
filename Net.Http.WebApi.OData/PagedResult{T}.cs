749e5be5b832fd06d6fcd0873a69837237a0ff65
Trevor Pilley <trev@trevorpilley.co.uk>
2017-03-02 13:57:37 +0000

    Replaced InlineCount<T> with PagedResult<T>
    The inline count result class was still returning OData 2.0 format {
    "__count":0, "results":[] }, updated to OData 3.0 format {
    "odata.count":0, "value":[] }

--- Net.Http.WebApi.OData/PagedResult{T}.cs
+++ Net.Http.WebApi.OData/PagedResult{T}.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="InlineCount{T}.cs" company="Project Contributors">
+// <copyright file="PagedResult{T}.cs" company="Project Contributors">
 // Copyright 2012 - 2017 Project Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,24 +15,24 @@ namespace Net.Http.WebApi.OData
     using System.Collections.Generic;
 
     /// <summary>
-    /// A class which is used to return results with an inline count.
+    /// A class which is used to return paged results with an overall result count.
     /// </summary>
     /// <typeparam name="T">The type of item contained in the collection.</typeparam>
     [System.Runtime.Serialization.DataContract]
-    public sealed class InlineCount<T>
+    public sealed class PagedResult<T>
     {
-        [System.Runtime.Serialization.DataMember(Name = "__count", Order = 0)]
+        [System.Runtime.Serialization.DataMember(Name = "odata.count", Order = 0)]
         private readonly int count;
 
-        [System.Runtime.Serialization.DataMember(Name = "results", Order = 1)]
+        [System.Runtime.Serialization.DataMember(Name = "value", Order = 1)]
         private readonly IList<T> results;
 
         /// <summary>
-        /// Initialises a new instance of the <see cref="InlineCount{T}"/> class.
+        /// Initialises a new instance of the <see cref="PagedResult{T}"/> class.
         /// </summary>
         /// <param name="results">The items to be returned.</param>
         /// <param name="count">The total result count.</param>
-        public InlineCount(IList<T> results, int count)
+        public PagedResult(IList<T> results, int count)
         {
             this.results = results;
             this.count = count;
