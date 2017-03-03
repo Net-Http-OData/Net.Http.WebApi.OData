ad53edb359a1044755bcb982e07a64810ff1a756
Trevor Pilley <trev@trevorpilley.co.uk>
2017-03-03 16:00:30 +0000

    Added a CreateODataResponse extension method

---
+++ Net.Http.WebApi.OData/ODataHeaderNames.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ODataHeaderNames.cs" company="Project Contributors">
+// Copyright 2012 - 2017 Project Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Net.Http.WebApi.OData
+{
+    /// <summary>
+    /// The header names for OData HTTP Headers
+    /// </summary>
+    public static class ODataHeaderNames
+    {
+        /// <summary>
+        /// The data service version header.
+        /// </summary>
+        public const string DataServiceVersion = "DataServiceVersion";
+    }
+}
