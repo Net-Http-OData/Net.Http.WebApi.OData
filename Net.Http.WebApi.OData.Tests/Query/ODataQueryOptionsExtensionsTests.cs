b03a1a44c668aa38b294b8160ddc93d9c6fd4826
Trevor Pilley <trev@trevorpilley.co.uk>
2017-05-09 12:28:20 +0100

    Added an extension method to calculate the next link

---
+++ Net.Http.WebApi.OData.Tests/Query/ODataQueryOptionsExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿namespace Net.Http.WebApi.OData.Tests.Query
+{
+    using System;
+    using System.Net.Http;
+    using OData.Model;
+    using OData.Query;
+    using Xunit;
+
+    public class ODataQueryOptionsExtensionsTests
+    {
+        [Fact]
+        public void NextLink_WithAllQueryOptions()
+        {
+            TestHelper.EnsureEDM();
+
+            var queryOptions = new ODataQueryOptions(
+                new HttpRequestMessage(HttpMethod.Get, "http://services.odata.org/OData/Customers?$expand=Category&$filter=Name eq 'Milk'&$format=json&$inlinecount=allpages&$orderby=Name&$select=Name,Price&$skip=25&$top=25"),
+                EntityDataModel.Current.EntitySets["Customers"]);
+
+            Assert.Equal(new Uri("http://services.odata.org/OData/Customers?skip=75&$expand=Category&$filter=Name eq 'Milk'&$format=json&$inlinecount=allpages&$orderby=Name&$select=Name,Price&$top=25"), queryOptions.NextLink(50, 25));
+        }
+
+        [Fact]
+        public void NextLink_WithNoQueryOptions()
+        {
+            TestHelper.EnsureEDM();
+
+            var queryOptions = new ODataQueryOptions(
+                new HttpRequestMessage(HttpMethod.Get, "http://services.odata.org/OData/Customers"),
+                EntityDataModel.Current.EntitySets["Customers"]);
+
+            Assert.Equal(new Uri("http://services.odata.org/OData/Customers?skip=75"), queryOptions.NextLink(50, 25));
+        }
+    }
+}
---
+++ Net.Http.WebApi.OData/Query/ODataQueryOptionsExtensions.cs
@@ -0,0 +1,79 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ODataQueryOptionsExtensions.cs" company="Project Contributors">
+// Copyright 2012 - 2017 Project Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Net.Http.WebApi.OData.Query
+{
+    using System;
+    using System.Text;
+
+    /// <summary>
+    /// Extensions for the <see cref="ODataQueryOptions"/> class
+    /// </summary>
+    public static class ODataQueryOptionsExtensions
+    {
+        /// <summary>
+        /// Gets the next link for a paged OData query.
+        /// </summary>
+        /// <param name="queryOptions">The query options.</param>
+        /// <param name="skip">The skip.</param>
+        /// <param name="resultsPerPage">The results per page.</param>
+        /// <returns>The next link for a paged OData query.</returns>
+        public static Uri NextLink(this ODataQueryOptions queryOptions, int skip, int resultsPerPage)
+        {
+            var requestUri = queryOptions.Request.RequestUri;
+
+            var uriBuilder = new StringBuilder()
+                .Append(requestUri.Scheme)
+                .Append(Uri.SchemeDelimiter)
+                .Append(requestUri.Authority)
+                .Append(requestUri.LocalPath)
+                .Append("?skip=").Append((skip + resultsPerPage).ToString());
+
+            if (queryOptions.RawValues.Expand != null)
+            {
+                uriBuilder.Append('&').Append(queryOptions.RawValues.Expand);
+            }
+
+            if (queryOptions.RawValues.Filter != null)
+            {
+                uriBuilder.Append('&').Append(queryOptions.RawValues.Filter);
+            }
+
+            if (queryOptions.RawValues.Format != null)
+            {
+                uriBuilder.Append('&').Append(queryOptions.RawValues.Format);
+            }
+
+            if (queryOptions.RawValues.InlineCount != null)
+            {
+                uriBuilder.Append('&').Append(queryOptions.RawValues.InlineCount);
+            }
+
+            if (queryOptions.RawValues.OrderBy != null)
+            {
+                uriBuilder.Append('&').Append(queryOptions.RawValues.OrderBy);
+            }
+
+            if (queryOptions.RawValues.Select != null)
+            {
+                uriBuilder.Append('&').Append(queryOptions.RawValues.Select);
+            }
+
+            if (queryOptions.RawValues.Top != null)
+            {
+                uriBuilder.Append('&').Append(queryOptions.RawValues.Top);
+            }
+
+            return new Uri(uriBuilder.ToString());
+        }
+    }
+}
