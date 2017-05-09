31030522b6796371afeb84cb9b3a66aa4bee4cc7
Trevor Pilley <trev@trevorpilley.co.uk>
2017-05-09 16:58:38 +0100

    Fixed bug in NextLink method
    The skip query option should be $skip

--- Net.Http.WebApi.OData.Tests/Query/ODataQueryOptionsExtensionsTests.cs
+++ Net.Http.WebApi.OData.Tests/Query/ODataQueryOptionsExtensionsTests.cs
@@ -17,7 +17,7 @@
                 new HttpRequestMessage(HttpMethod.Get, "http://services.odata.org/OData/Customers?$expand=Category&$filter=Name eq 'Milk'&$format=json&$inlinecount=allpages&$orderby=Name&$select=Name,Price&$skip=25&$top=25"),
                 EntityDataModel.Current.EntitySets["Customers"]);
 
-            Assert.Equal(new Uri("http://services.odata.org/OData/Customers?skip=75&$expand=Category&$filter=Name eq 'Milk'&$format=json&$inlinecount=allpages&$orderby=Name&$select=Name,Price&$top=25"), queryOptions.NextLink(50, 25));
+            Assert.Equal(new Uri("http://services.odata.org/OData/Customers?$skip=75&$expand=Category&$filter=Name eq 'Milk'&$format=json&$inlinecount=allpages&$orderby=Name&$select=Name,Price&$top=25"), queryOptions.NextLink(50, 25));
         }
 
         [Fact]
@@ -29,7 +29,7 @@
                 new HttpRequestMessage(HttpMethod.Get, "http://services.odata.org/OData/Customers"),
                 EntityDataModel.Current.EntitySets["Customers"]);
 
-            Assert.Equal(new Uri("http://services.odata.org/OData/Customers?skip=75"), queryOptions.NextLink(50, 25));
+            Assert.Equal(new Uri("http://services.odata.org/OData/Customers?$skip=75"), queryOptions.NextLink(50, 25));
         }
     }
 }
