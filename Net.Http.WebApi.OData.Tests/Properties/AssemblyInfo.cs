596e39287fc7651b82eb88b0ad5194213e680d39
Trevor Pilley <trev@trevorpilley.co.uk>
2018-01-15 13:23:17 +0000

    Updated assembly info to 4.0.2 for next release

--- Net.Http.WebApi.OData.Tests/Properties/AssemblyInfo.cs
+++ Net.Http.WebApi.OData.Tests/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using Xunit;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.2.0")]
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
