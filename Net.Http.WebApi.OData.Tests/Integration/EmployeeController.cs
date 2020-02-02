52dfe022305bc96560c51d3da6f0176e7dd98dff
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2020-02-02 16:48:01 +0000

    Added an additional integration test to ensure the ODataQueryOptionsHttpParameterBinding works in the pipeline with the ODataRequestDelegatingHandler

---
+++ Net.Http.WebApi.OData.Tests/Integration/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Net.Http.OData.Query;
+using NorthwindModel;
+
+namespace Net.Http.WebApi.OData.Tests.Integration
+{
+    /// <summary>
+    /// An implementation for integration tests against an entity set.
+    /// </summary>
+    [RoutePrefix("odata/Employees")]
+    public sealed class EmployeeController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult Get(ODataQueryOptions queryOptions)
+        {
+            IEnumerable<Employee> value = Enumerable.Empty<Employee>();
+            string context = Request.ResolveODataContext(queryOptions.EntitySet, queryOptions.Select);
+
+            var responseContent = new ODataResponseContent(value, context);
+
+            return Ok(responseContent);
+        }
+    }
+}
---
+++ Net.Http.WebApi.OData.Tests/Integration/GetQueryOptionsTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Net.Http.OData;
+using Xunit;
+
+namespace Net.Http.WebApi.OData.Tests.Integration
+{
+    public class GetQueryOptionsTests : IntegrationTest
+    {
+        private readonly HttpResponseMessage _httpResponseMessage;
+
+        public GetQueryOptionsTests()
+        {
+            _httpResponseMessage = HttpClient.GetAsync("http://server/odata/Employees").Result;
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void Contains_Header_ContentType_ApplicationJson()
+        {
+            Assert.Equal("application/json", _httpResponseMessage.Content.Headers.ContentType.MediaType);
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void Contains_Header_ContentType_Parameter_ODataMetadata()
+        {
+            Assert.Equal("minimal", _httpResponseMessage.Content.Headers.ContentType.Parameters.Single(x => x.Name == ODataMetadataLevelExtensions.HeaderName).Value);
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void Contains_Header_ODataVersion()
+        {
+            Assert.Equal(ODataVersion.OData40.ToString(), _httpResponseMessage.Headers.GetValues(ODataHeaderNames.ODataVersion).Single());
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public async Task Contains_ODataResponseContent()
+        {
+            Assert.NotNull(_httpResponseMessage.Content);
+
+            string result = await _httpResponseMessage.Content.ReadAsStringAsync();
+
+            Assert.Equal(
+                "{\"@odata.context\":\"http://server/odata/$metadata#Employees\",\"value\":[]}",
+                result);
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void StatusCode_OK()
+        {
+            Assert.Equal(HttpStatusCode.OK, _httpResponseMessage.StatusCode);
+        }
+    }
+}
